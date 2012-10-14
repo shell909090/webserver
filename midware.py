05bbf65793326e509ab4a27e868d20bf7f64a64a
shell909090@debox <shell909090@gmail.com>
2012-10-14 21:02:43 +0800

    * 将http中的header调整为raw模式，http.py不再转换大小写

--- midware.py
+++ midware.py
@@ -34,7 +34,7 @@ class Cache(object):
                 return cPickle.loads(pd)
             res = func(req)
             if res is not None and res.cache and hasattr(res, 'body'):
-                res.set_header('cache-control', 'max-age=%d' % res.cache)
+                res.set_header('Cache-Control', 'max-age=%d' % res.cache)
                 pd = cPickle.dumps(res, 2)
                 self.set_data(req.url.path, pd, res.cache)
             return res
@@ -133,14 +133,14 @@ class Cookie(object):
         self.v[k] = v
     def set_cookie(self, res):
         for k in self.m:
-            res.add_header('set-cookie', '%s=%s' % (k, urllib.quote(self.v[k])))
+            res.add_header('Set-Cookie', '%s=%s' % (k, urllib.quote(self.v[k])))
 
 class Session(object):
     def __init__(self, timeout): self.exp = timeout
 
     def __call__(self, func):
         def inner(req):
-            req.cookie = Cookie(req.get_header('cookie', None))
+            req.cookie = Cookie(req.get_header('Cookie', None))
             sessionid = req.cookie.get('sessionid', '')
             if not sessionid:
                 sessionid = get_rnd_sess()
