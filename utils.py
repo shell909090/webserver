506cb4c5d78a043d34bf773c16b80f551a9b1ff3
Shell.Xu <shell909090@gmail.com>
2014-02-20 14:57:05 +0800

    * bug fix

--- utils.py
+++ utils.py
@@ -8,17 +8,15 @@ import logging
 
 LOGFMT = '%(asctime)s.%(msecs)03d[%(levelname)s](%(module)s:%(lineno)d): %(message)s'
 def initlog(lv, logfile=None, stream=None, longdate=False):
-    if logfile.startswith('syslog:'):
-        import logging.handlers
-        handler = logging.handlers.SysLogHandler(logfile[7:])
+    if logfile and logfile.startswith('syslog:'):
+        from logging import handlers
+        handler = handlers.SysLogHandler(logfile[7:])
     elif logfile: handler = logging.FileHandler(logfile)
     elif stream: handler = logging.StreamHandler(stream)
     else: handler = logging.StreamHandler(sys.stderr)
-
     datefmt = '%H:%M:%S'
     if longdate: datefmt = '%Y-%m-%d %H:%M:%S'
     handler.setFormatter(logging.Formatter(LOGFMT, datefmt))
-
     logger = logging.getLogger()
     if isinstance(lv, basestring): lv = getattr(logging, lv)
     logger.setLevel(lv)
