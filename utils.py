3b9fb9f6f56f1b5a70e95c79302ecbd5e3c1a7c2
Shell.Xu <shell909090@gmail.com>
2014-02-20 14:52:42 +0800

    * 修改initlog，在已经产生rootlog的情况下能够工作

--- utils.py
+++ utils.py
@@ -8,12 +8,21 @@ import logging
 
 LOGFMT = '%(asctime)s.%(msecs)03d[%(levelname)s](%(module)s:%(lineno)d): %(message)s'
 def initlog(lv, logfile=None, stream=None, longdate=False):
+    if logfile.startswith('syslog:'):
+        import logging.handlers
+        handler = logging.handlers.SysLogHandler(logfile[7:])
+    elif logfile: handler = logging.FileHandler(logfile)
+    elif stream: handler = logging.StreamHandler(stream)
+    else: handler = logging.StreamHandler(sys.stderr)
+
+    datefmt = '%H:%M:%S'
+    if longdate: datefmt = '%Y-%m-%d %H:%M:%S'
+    handler.setFormatter(logging.Formatter(LOGFMT, datefmt))
+
+    logger = logging.getLogger()
     if isinstance(lv, basestring): lv = getattr(logging, lv)
-    kw = {'format': LOGFMT, 'datefmt': '%H:%M:%S', 'level': lv}
-    if logfile: kw['filename'] = logfile
-    if stream: kw['stream'] = stream
-    if longdate: kw['datefmt'] = '%Y-%m-%d %H:%M:%S'
-    logging.basicConfig(**kw)
+    logger.setLevel(lv)
+    logger.addHandler(handler)
 
 def getcfg(cfgpathes):
     from ConfigParser import SafeConfigParser
